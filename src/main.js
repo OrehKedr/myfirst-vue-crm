333bda1c51e359e415ea97e4c8134ba58df2dce7
Alexandr Prokofyev <alprokoviev@gmail.com>
2020-05-16 13:58:57 +0300

    Первоначальная структура приложения

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// Подключаем скрипты для различных плагинов, анимаций, и т.д.
+// В этом импорте не используем псевдоним '~', 
+// такой импорт на стороне сервера по умолчанию ищет файлы в папке node_modules.
+import 'materialize-css/dist/js/materialize.min'
+
 Vue.config.productionTip = false
 
 new Vue({
